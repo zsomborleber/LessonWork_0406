b3fee6aa42a00e2816b3e7ff7de8b53200622610
Zsombor Leber <leberzsombor96@gmail.com>
2022-04-06 17:56:19 +0200

    test írás

--- src/test/java/main/WithoutTestTest.java
+++ src/test/java/main/WithoutTestTest.java
@@ -44,17 +44,42 @@ class WithoutTestTest {
 
     @Test
     void test_checkGender() {
-        // write your test cases here
+        String result1 = WithoutTest.checkGender(0);
+        assertEquals("nő", result1);
+
+        String result2 = WithoutTest.checkGender(1);
+        assertEquals("férfi", result2);
+
+        String reuslt3 = WithoutTest.checkGender(-1);
+        assertEquals("valami más", reuslt3);
     }
 
     @Test
     void test_trafficLight() {
-        // write your test cases here
+        String result1 = WithoutTest.trafficLight("zöld");
+        assertEquals("SZABAD", result1);
+
+        String result2 = WithoutTest.trafficLight("sárga");
+        assertEquals("SÁRGA", result2 );
+
+        String result3 = WithoutTest.trafficLight("piros");
+        assertEquals("MEGÁLLJ", result3);
+
+        String other = WithoutTest.trafficLight("a");
+        assertEquals("NEM TUDOM", other);
     }
 
     @Test
     void test_isLeapYear() {
-        // write your test cases here
+        assertTrue(WithoutTest.isLeapYear(1584));
+        assertTrue(WithoutTest.isLeapYear(1600));
+        assertTrue(WithoutTest.isLeapYear(1996));
+        assertTrue(WithoutTest.isLeapYear(2024));
+
+        assertFalse(WithoutTest.isLeapYear(1200));
+        assertFalse(WithoutTest.isLeapYear(1580));
+        assertFalse(WithoutTest.isLeapYear(1800));
+        assertFalse(WithoutTest.isLeapYear(20201));
     }
 
 }
